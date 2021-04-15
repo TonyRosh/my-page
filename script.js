0668105f234b11dc3dc62829a515ba35a054d30c
Tony Roscioli <adrprogramming@gmail.com>
2021-04-15 14:50:07 -0600

    added javascript to change size of pictures

---
+++ script.js
@@ -0,0 +1,15 @@
+face.addEventListener('mouseover',function(event){
+    face.style.width = '200px';
+    face.style.height = '200px'})
+
+pup.addEventListener('mouseover',function(event){
+    pup.style.width = '200px';
+    pup.style.height = '200px'})
+
+face.addEventListener('mouseout',function(event){
+    face.style.width = '50px';
+    face.style.height = '50px'})
+    
+pup.addEventListener('mouseout',function(event){
+    pup.style.width = '50px';
+    pup.style.height = '50px'})
